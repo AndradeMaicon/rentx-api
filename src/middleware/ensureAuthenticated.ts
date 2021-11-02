0974e85528a389c757e6d2128f820f298ee56951
Maicon <andrade.maicon.pr@gmail.com>
2021-11-02 19:46:34 -0300

    chore: Creating authentication middleware

---
+++ src/middleware/ensureAuthenticated.ts
@@ -0,0 +1,43 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+
+import { AppError } from '../errors/AppError';
+import { UsersRepository } from '../modules/accounts/repositories/implementations/UsersRepository';
+
+interface IPayload {
+  sub: string;
+}
+
+async function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction
+) {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('Token missing', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { sub: user_id } = verify(
+      token,
+      '05db08ea7d483b1feb97e187bc949ece'
+    ) as IPayload;
+
+    const userRepository = new UsersRepository();
+    const user = userRepository.findById(user_id);
+
+    if (!user) {
+      throw new AppError('User does not exists!', 401);
+    }
+
+    next();
+  } catch {
+    throw new AppError('Invalid token', 401);
+  }
+}
+
+export { ensureAuthenticated };
